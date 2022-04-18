96faad197e78a09d02e4f3cb9d4a6c6029b317a4
Martia <pureromancebymartia@gmail.com>
2022-04-18 19:28:52 -0400

    welcome

---
+++ app.js
@@ -0,0 +1,41 @@
+
+//#1
+const myArray = [];
+
+ for (var i = 1 ; i < 11; i++) {
+  myArray.push(i);
+}
+
+//#2
+
+const myArray = [];
+
+let i = 1;
+
+while (i <= 10 ) {
+  myArray.push(i);
+  i++;
+}
+
+
+
+
+
+//#3
+
+const myArray = [];
+
+ for (var i = 11 ; i < 22; i+= 2) {
+  myArray.push(i);
+}
+
+
+
+//#4
+
+const myArray = [];
+
+ for (var i = 15  ; i > 0; i-= 2) {
+  myArray.push(i);
+}
+
